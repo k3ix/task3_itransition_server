44fad41e47c753321f7a4a6af6b6087a58c1b4aa
k3ix <egormishuk@gmail.com>
2022-02-11 16:36:42 +0300

    fin_changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -10,9 +11,15 @@ const db = require('./models');
 const usersRouter = require('./routes/Users');
 app.use("/users", usersRouter);
 
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
+db.sequelize
+    .sync()
+    .then(() => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log('Server is running on port 3001');
     });
-});
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 
